fcf2c71be35ae1849e3e0c71a87075b149ab28e0
wildanmirtaza <wildanmirtaza@gmail.com>
2025-10-13 01:11:53 +0700

    tulisan di tab browser

--- src/pages/undanganPages.jsx
+++ src/pages/undanganPages.jsx
@@ -333,30 +333,6 @@ function App() {
                                 animate="visible"
                             />
                             <motion.div
-                                className="mb-8 text-lg space-y-1 font-poppins"
-                                variants={fadeIn}
-                                initial="hidden"
-                                animate="visible"
-                            >
-                                <p></p>
-                            </motion.div>
-                            <motion.div
-                                className="mb-8 text-lg space-y-1 font-poppins"
-                                variants={fadeIn}
-                                initial="hidden"
-                                animate="visible"
-                            >
-                                <p></p>
-                            </motion.div>
-                            <motion.div
-                                className="mb-8 text-lg space-y-1 font-poppins"
-                                variants={fadeIn}
-                                initial="hidden"
-                                animate="visible"
-                            >
-                                <p></p>
-                            </motion.div>
-                            <motion.div
                                 className="mb-8 text-lg space-y-1 font-poppins font-semibold"
                                 variants={fadeIn}
                                 initial="hidden"
@@ -380,6 +356,14 @@ function App() {
                                 animate="visible"
                             >
                                 <p>Dresscode</p>
+                                <p>???</p>
+                            </motion.div>
+                            <motion.div
+                                className="mb-8 text-lg space-y-1 font-poppins"
+                                variants={fadeIn}
+                                initial="hidden"
+                                animate="visible"
+                            >
                                 <p></p>
                             </motion.div>
                             <motion.div
